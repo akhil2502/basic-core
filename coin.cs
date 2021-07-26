1556528a056d14784a79f5768662b88521cb4272
akhil2502 <akhilachilies>
2021-07-26 19:07:00 +0530

    Added Assignment-5

---
+++ coin.cs
@@ -0,0 +1,62 @@
+﻿namespace FlippingCoin
+
+{
+
+    class Program
+
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Let's flip a coin 10000 times");
+
+            Coinflip();
+
+        }
+
+
+
+        private static int Coinflip()
+
+        {
+            Random Rand = new Random();
+
+            int heads = 0;
+
+            int tails = 0;
+
+            int result = 0;
+
+            for (int i = 0; i < 10000; i++)
+
+            {
+
+                result = Rand.Next(0, 2);
+
+
+
+                if (result == 1)
+                {
+
+                    heads++;
+
+                }
+                else
+                {
+                    tails++;
+
+
+                }
+            }
+            Console.WriteLine("heads was flipped {0} times", heads);
+
+            Console.WriteLine("tails was flipped {0} times", tails);
+
+            return result;
+        }
+
+    }
+}
